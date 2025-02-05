9233e5e1fa6a95972e704f0e9aead5a6ee28ea06
Jose77-12 <jbustillosviera@gmail.com>
2025-02-05 11:34:48 -0500

    Actualizar rutas de la API de citas para simplificar la creación y actualización de citas, eliminando parámetros innecesarios en las rutas.

--- src/routes/citasRoutes.js
+++ src/routes/citasRoutes.js
@@ -4,13 +4,13 @@ import controller from '../controllers/citasController.js'
 const router = Router()
 
 // Crear una nueva cita
-router.post('/appointments/:fecha/:hora/:cliente/:descripcion', controller.createCita);
+router.post('/appointments', controller.createCita);
 
 // Consultar una cita específica
 router.get('/appointments/:id', controller.getCita);
 
 // Actualizar detalles de una cita
-router.put('/appointments/:id/:fecha/:hora/:cliente/:descripcion', controller.updateCita);
+router.put('/appointments/:id', controller.updateCita);
 
 // Eliminar una cita
 router.delete('/appointments/:id', controller.deleteCita);
